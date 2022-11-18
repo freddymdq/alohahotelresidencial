9b63793eef86721e68311be97a04134c29ac0323
Fede Mosquera <freddymdq@gmail.com>
2022-11-18 13:20:03 -0300

    listo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <ul>
+          <li>INICIO</li>
+          <li>GALERIA</li>
+          <li>SERVICIO</li>
+          <li>CONTACTO</li>
+        </ul>
+        <h1>Sistema de Reservas</h1>
+        
+    </div>
+  );
+}
+
+export default App;
